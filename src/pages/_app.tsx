7200d18fd2d74534897c14470db154ac1a5bdc91
Brandon Birkholz <brandonjbirkholz@gmail.com>
2025-02-01 19:23:26 -0600

    Add Vercel analytics & speed insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
-import "../styles/globals.css"
-import { ThemeProvider } from "../components/theme-provider"
 import { TooltipProvider } from "@components/ui/tooltip"
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { AppProps } from "next/app"
+import { ThemeProvider } from "../components/theme-provider"
+import "../styles/globals.css"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -11,6 +13,8 @@ export default function App({ Component, pageProps }: AppProps) {
       enableSystem={false}
       disableTransitionOnChange
     >
+      <Analytics />
+      <SpeedInsights />
       <TooltipProvider delayDuration={0}>
         <Component {...pageProps} />
       </TooltipProvider>
